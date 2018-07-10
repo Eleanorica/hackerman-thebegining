c21b9d40255e90d7a8544a19fd3850f6d6286630
Eleanorica <40950616+Eleanorica@users.noreply.github.com>
2018-07-10 10:21:50 -0500

    Add files via upload

--- proj02_loops/proj02_02.py
+++ proj02_loops/proj02_02.py
@@ -10,4 +10,63 @@ previous two numbers in the sequence. The sequence looks like this:
 1, 1, 2, 3, 5, 8, 13...
 """
 
+#key word is "for"
+#loop over a range of numbers
 
+# sum = 0
+# for num in range (0,9):
+#     sum = sum + num
+#     print sum
+
+
+#loop over strings
+
+# s = "vsa"
+# for letter in s:
+#     print letter
+#     if letter == "s":
+#         print "This is an s"
+
+
+# prev = 0
+# curr = 1
+# nxt = 1
+# thismany = raw_input("How many Fibonacci numbers should I generate?")
+# for num in range (0,int(thismany)):
+#     print curr
+#     nxt = curr + prev
+#     prev = curr
+#     curr = nxt
+
+
+# s = 0
+# p = 0
+# c = 1
+# n = 1
+# thismany = raw_input("How many Fibonacci numbers should I generate?")
+# while s < int(thismany):
+#     print c
+#     n = c + p
+#     p = c
+#     c = n
+#     s = s + 1
+
+
+# prev = 0
+# curr = 1
+# thismany = raw_input("How many Fibonacci numbers should I generate?")
+# for num in range (0,int(thismany)):
+#     print curr
+#     curr = curr + prev
+#     prev = curr - prev
+
+
+prev = 0
+curr = 1
+nxt = 1
+thismany = raw_input("How many Fibonacci numbers should I generate?")
+for num in range (0,int(thismany)):
+    print curr
+    nxt = curr + prev
+    prev = curr
+    curr = nxt
